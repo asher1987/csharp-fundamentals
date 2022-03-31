073c71a2b6269c90a539b5d5cc48675531bf406f
Ashley Likes <alikes@wsutech.edu>
2022-03-31 14:44:47 -0500

    lesson template

---
+++ MyFirstApplication/Template for Lessons.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace MyFirstApplication;
+
+/*
+ The class description describes the Exercise Lab from a class level.
+ The class name should reflect the lab name... Exercise1. To get credit you
+need to provide the direct link to the exercise file from GitHub. If the
+instructions ask you to create more than 1 file, include links for each file.
+ */
+public class ExerciseTemplate
+{
+
+    /*
+     Include comments like this before each exercise number. The comment should include
+     the exercise number and its information. If more than one method is required, this will
+    be written once for that exercise number. The method name will be defined
+    in the lab.
+     */
+    public void ExerciseMethod()
+    {
+
+    }
+
+} // end class
