2a19c1c0f48a4be7253a65041d18ebccea8f540e
Ashley Likes <alikes@wsutech.edu>
2022-04-07 15:14:07 -0500

    x

---
+++ MyFirstApplication/Lessons/Lesson2.cs
@@ -0,0 +1,75 @@
+﻿
+namespace MyFirstApplication;
+
+/*
+This class goes over Strings
+*/
+
+internal class Lesson2
+{
+    string myString = "Hello World";
+
+    // String Method-Trim
+    public void MyTrimExample()
+    {
+        string str = " Word ";
+        // before trim
+        Console.WriteLine(str);
+        //trim
+        string myTrim = str.Trim();
+        // after trim
+        Console.WriteLine(myTrim);
+    }
+
+    // String method - equals
+    public void MyEqualsExample(string beta)
+    {
+        Console.WriteLine(beta.Equals("World"));
+    }
+
+    // String Methods To Upper (capitalize) to Lower
+    public void MyUpperLowerExample()
+    {
+        Console.WriteLine(myString.ToUpper());
+        Console.WriteLine(myString.ToLower());
+
+    }
+
+
+    // String Method- Length
+    public int MyStringLength()
+    {
+        return myString.Length;
+
+
+    }
+
+    // Char- uses single quotes to identify it. Case sensitive
+    public void MyExampleChar()
+    {
+        char myChar = 't';
+        char copyright = '\u00A9';
+        char something = '\x0089';
+        Console.WriteLine(myChar);
+        Console.WriteLine(copyright);
+        Console.WriteLine(something);
+    }
+
+    // Escape Sequence
+    public string MyEscapeExample()
+    {
+        return "That\'s a cool car. \t Can I drive it something?";
+    }
+
+
+
+
+
+} // end class
+
+
+
+
+
+
+
--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -68,10 +68,44 @@ MyLocalFunction();
 void Exercise2()
 {
     Exercise2 myExercise2 = new Exercise2();
-    myExercise2.Question1();
+    myExercise2.Question1("Exercise2 Lab");
     myExercise2.Question2();
     myExercise2.Question3();
 
     Console.WriteLine(myExercise2.Question4());
 }
 Exercise2();
+
+Lesson3Example();
+
+
+//Lesson 3 Operators and Overflow Checking
+void Lesson3Example()
+{
+    Lesson3 myLesson3 = new Lesson3();
+    //myLesson3.BasicCheckedOperator(14);
+    //myLesson3.BasicCheckedExample2();
+    // int alpha = 2147483647;
+    //int total = alpha + 10;
+    // Console.WriteLine(total);
+    // -2147483639 9 positions away from reaching the max/min
+    //myLesson3.BasicUncheckedExample();
+    //myLesson3.BasicMath();
+    //myLesson3.BasicModulus(783);
+    // myLesson3.BasicBooleanLogical(true, false);
+    //myLesson3.BasicCompoundExample(12, 35, 9);
+   // myLesson3.BasicRelationalExample("Tom", "Tom");
+}
+
+void Question4()
+{
+    Exercise3 myExercise3 = new Exercise3();
+    myExercise3.Question4(false, true);
+    myExercise3.Question4(false, false);
+    myExercise3.Question4(true, true);
+    myExercise3.Question4(true, false);
+
+
+
+}
+Question4();
