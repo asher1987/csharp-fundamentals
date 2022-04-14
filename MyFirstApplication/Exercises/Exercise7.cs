20c021a8a4990a868eccb57692c052f5ed8a3833
Ashley Likes <alikes@wsutech.edu>
2022-04-14 11:49:58 -0500

    Exercise 7

---
+++ MyFirstApplication/Exercises/Exercise7.cs
@@ -0,0 +1,52 @@
+﻿namespace MyFirstApplication;
+
+
+/*
+1. Change the type from class to struct. Change the name of the struct from 
+Exercise7 to Employee. The filename should not be changed. Create 2 
+struct variables. The first one will be of type int and be named _id. The 
+second variable should be of type string and be named _name. Create a 
+constructor that takes 2 arguments, with the first being an int and the 
+second a string. The struct variables should be assigned to the constructor 
+parameters. Create properties for these 2 variables.
+ */
+
+internal struct Employee
+{
+    private int _id;
+    private string _name;
+
+    public int employeeId
+     {
+     get { return _id; }
+     
+     }
+
+     public string employeeName
+      {
+      get { return _name; }
+      // set { _name = value; }
+      }
+
+      public Employee(int employeeId, string employeeName)
+        {
+            _id = employeeId;
+            _name = employeeName;
+        }
+
+    }
+/*
+2. Create a Record based on your Boat object from the Object Oriented 
+Programming lesson. In your record body, include the boat method that 
+you defined. This record can be in the same file as exercise 1. Just put the 
+code under it as seen in the lecture. Test your record to ensure it is working 
+correctly
+ */
+
+internal record Boat (string speed)
+{
+    public void GoesFast()
+    {
+        Console.WriteLine(speed);
+    }
+}
