8240c9a29924dff5c7656090a08f38a5aa7c0762
Ashley Likes <alikes@wsutech.edu>
2022-04-19 14:24:34 -0500

    Exercise 11 update

--- MyFirstApplication/Exercises/Exercise11.cs
+++ MyFirstApplication/Exercises/Exercise11.cs
@@ -8,14 +8,14 @@
  * parameters in the parenthesis. In the body of the constructor 
  * you will assign your child parameter to your property.
  * 
- * The second constructorshould have 2 parameters, 2 based on the horse and 
+ * The second constructor should have 2 parameters, 2 based on the horse and 
  * 1 from your child. Using the :this()keyword, you will pass 2 of the 
  * parent attributes and your child attribute information to the first 
  * constructor.Include default values to represent the 1 remaining horse 
  * parameters
  * 
  * For the last constructor, you will create the default constructor. 
- * Using the :this()keyword, you will pass 2default valuesin that will 
+ * Using the :this()keyword, you will pass 2 default values in that will 
  * pass to the 2ndconstructor. Also include in the class, your method for your 
  * child class.
 
@@ -31,20 +31,47 @@ internal class Pony : Horse
         : base(color, weight, size)
     {
         Ponyage = age;
-        Horsecolor = color;
-        Horseweight = weight;
-        Horsesize = size;
+        
     }
 
     //constructor 2
-    public Pony(string color, int age)
+    public Pony(string color, int weight, int age)
     
-     :this(color, 700, "small", age)
-            {
+     :this(color, weight, "small", age)
+    {
 
-        }
+    }     
+
+        
     // third constructor-default
     public Pony()
-        : this("Brown", 700, "small", 5) { }
+        : this("Brown", 700, 5) { }
     }
+
+
+internal class Fishing_Boat : Boat
+{
+    //auto property
+    public int Topspeed { get; init; }
+
+    //constructor 1
+    public Fishing_Boat(string color, int weight, string size, int speed)
+    {
+        Topspeed = speed;
+    }
+
+    //constructor 2
+    public Fishing_Boat(string color, int weight, int speed)
+        :this(color, weight, "medium", speed)
+    {
+
+    }
+
+    // third constructor
+    public Fishing_Boat()
+        :this("Red", 5000, 150) { }
+    {
+
+    }
+}
 
